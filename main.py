25b20506d731f5621d698f6979b57cad6c28118f
Carlin Liao <liaoc2@mskcc.org>
2022-10-24 17:43:27 -0400

    updated main to match last changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 from os import makedirs
 from shutil import rmtree
 from argparse import ArgumentParser
+from typing import Tuple, List
 
-from numpy import savetxt
+from dgl import DGLGraph
 
 from hactnet.spt_to_file import spt_to_dataframes
 from hactnet.generate_graph_from_spt import generate_graphs
@@ -81,14 +82,6 @@ def parse_arguments():
         required=False
     )
     parser.add_argument(
-        '-conf',
-        '--config_fpath',
-        type=str,
-        help='path to the config file.',
-        default='',
-        required=False
-    )
-    parser.add_argument(
         '-b',
         '--batch_size',
         type=int,
@@ -122,23 +115,53 @@ def parse_arguments():
         required=False,
         default=0
     )
+    parser.add_argument(
+        '--explainer',
+        type=str,
+        help='Which explainer type to use.',
+        default='pp',
+        required=False
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
     makedirs('tmp/', exist_ok=True)
-    df_feat, df_label = spt_to_dataframes(args.analysis_study, args.measurement_study,
-                                          args.specimen_study, args.host, args.dbname, args.user,
-                                          args.password)
-    sets_data, graph_to_label, graph_names = generate_graphs(df_feat, df_label, args.val_data_prc,
-                                                             args.test_data_prc,
-                                                             args.roi_side_length)
-    training_graphs = [g for g_list in sets_data[0].values() for g in g_list]
-    training_labels = [graph_to_label[g] for g in training_graphs]
-    model = train(args.config_fpath, 'tmp/', (training_graphs, training_labels), None,
-                  None, None, None, True, args.epochs, args.learning_rate, args.batch_size, args.k)
-    explanations = explain_cell_graphs(sets_data[0], model, None, None, None)
-    for graph, explanation in explanations.items():
-        savetxt(f'tmp/{graph_names[graph]}_node_importance.txt', explanation)
+    df_cell, df_label = spt_to_dataframes(
+        args.analysis_study, args.measurement_study, args.specimen_study, args.host, args.dbname,
+        args.user, args.password)
+    sets_data, graph_to_label, graph_names = generate_graphs(
+        df_cell, df_label, args.val_data_prc, args.test_data_prc, args.roi_side_length)
+    tuple_sets: List[Tuple[List[DGLGraph], List[int]]] = []
+    name_sets: List[List[str]] = []
+    for set_dict in sets_data:
+        graphs = [g for g_list in set_dict.values() for g in g_list]
+        labels = [graph_to_label[g] for g in graphs]
+        name_sets.append([graph_names[g] for g in graphs])
+        tuple_sets.append((graphs, labels))
+    assert len(tuple_sets) == 3
+    assert tuple_sets[0] is not None
+    train_val_test = (tuple_sets[0], tuple_sets[1], tuple_sets[2])
+    model = train(train_val_test, 'tmp/', logger=args.logger, epochs=args.epochs,
+                  learning_rate=args.learning_rate, batch_size=args.batch_size, k=args.k)
+    columns = df_cell.columns.values
+    i = -1
+    while len(train_val_test[i][0]) == 0:
+        i -= 1
+        if i < -3:
+            raise RuntimeError('all sets created are empty')
+    eval_set = train_val_test[i]
+    assert eval_set is not None
+    explanations = explain_cell_graphs(
+        eval_set, model, args.explainer,
+        [g.ndata['phenotypes'] for g in eval_set[0]],
+        [col[3:] for col in columns if col.startswith('PH_')],
+        feature_names=[col[3:] for col in columns if col.startswith('FT_')],
+        cell_graph_names=name_sets[i],
+        out_directory='out/')
+    explanations[0].to_csv('out/separability_concept.csv')
+    explanations[1].to_csv('out/separability_attribute.csv')
+    for class_pair, df in explanations[2].items():
+        df.to_csv(f'out/separability_k_best_{class_pair}.csv')
     rmtree('tmp/')
