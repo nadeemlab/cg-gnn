c2ce8b65a1904cfd327f76a7460a6dde45ac0a27
Carlin Liao <liaoc2@mskcc.org>
2022-11-01 12:32:58 -0400

    remove more legacy files

--- shell_scripts/generate_hact_graph_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-conda activate hact
-
-python /nadeem_lab/Carlin/hact-net/hact-net/generate_hact_graphs.py \
---image_path /nadeem_lab/data/external_downloads/BRACS_RoI/latest_version/train/ \
---save_path /nadeem_lab/Carlin/hact-net/data/hact-net-data-v6/ \
---disable_stain_norm
--- shell_scripts/train_tissue_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conda activate hact
-
-python /nadeem_lab/Carlin/hact-net/hact-net/train.py \
---cg_path /nadeem_lab/Carlin/hact-net/data/hact-net-data-v5/tissue_graphs/ \
---model_path /nadeem_lab/Carlin/hact-net/models/ \
---config_fpath /nadeem_lab/Carlin/hact-net/config/bracs_tggnn_7_classes_pna.yml \
--b 8 --in_ram --epochs 100 -l 0.0005
